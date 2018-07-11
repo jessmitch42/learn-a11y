1d39500982ab6f3dc905e5036333fb721b8ad79d
jessmitch42 <jessicamitch42@gmail.com>
2018-07-11 15:57:16 -0400

    tab trapping

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,43 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
+  var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary');
+  focusableElements = Array.prototype.slice.call(focusableElements);
   // Show the modal and overlay
+  var firstItem = focusableElements[0];
+  var lastItem = focusableElements[focusableElements.length-1]
+  var previouslyFocused = document.activeElement;
+
+  modal.addEventListener('keydown', trap);
+
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function trap(e) {
+    console.log(document.activeElement)
+    console.log(lastItem)
+    if (e.keyCode === 9) { //tab
+      // if shift is held down
+      if (e.shiftKey) {
+        //going backwards
+        if (document.activeElement === firstItem) {
+          e.preventDefault;
+          lastItem.focus();
+        }
+      }
+      // if shift is not held down
+      else {
+        if (document.activeElement === lastItem) {
+          e.preventDefault;
+          firstItem.focus();
+        }
+      }
+    }
+    else if (e.keyCode === 27) {
+      close();
+    }
+  }
+
 }
 
 function close() {
